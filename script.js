cb1fdc29fa6745d48b3bd442f08f9a2cb8ec3702
the.o <164587286+RifkideoWinada@users.noreply.github.com>
2025-05-24 16:27:00 +0700

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const navList = document.getElementById('nav-list');
 hamburger.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
+
+// Menampilkan tahun saat ini di footer
+const currentYear = document.getElementById('current-year');
+currentYear.textContent = new Date().getFullYear();
